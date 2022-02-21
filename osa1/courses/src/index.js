77a9cbd7ccab18874916fc588bdda34b5646427f
Ville Komulainen <ville.m.komulainen@utu.fi>
2022-02-21 22:04:07 +0200

    1.4

--- osa1/courses/src/index.js
+++ osa1/courses/src/index.js
@@ -21,47 +21,51 @@ import ReactDOM from 'react-dom'
   
 
   const Total = (props) => {
+    const t = props.parts
+    
     return(
       <>
-      <p>Total {props.amount} exercises</p>
+      <p>Total {t[0].exercises+t[1].exercises+t[2].exercises} exercises</p>
+      </>
+    )
+  }
+  const Contents = (props) => {
+    const t = props.parts
+    return(
+      <>
+      <Part part={t[0].name} exercises={t[0].exercises}/>
+      <Part part={t[1].name} exercises={t[1].exercises}/>
+      <Part part={t[2].name} exercises={t[2].exercises}/>
       </>
     )
   }
 
   
-const App = () => {
-  const course = 'Superadvanced web and mobile programming'
-  const part1 = {
-    name: 'Basics of React',
-    exercises: 8
-  }
-  const part2 = {
-    name: 'Using props',
-    exercises: 10
-  }
-  const part3 = {
-    name: 'Component states',
-    exercises: 12
-  }
-  const Contents = (props) => {
+  const App = () => {
+    const course = 'Superadvanced web and mobile programming'
+    const parts = [
+      {
+        name: 'Basics of React',
+        exercises: 8
+      },
+      {
+        name: 'Using props',
+        exercises: 10
+      },
+      {
+        name: 'Component states',
+        exercises: 12
+      }
+    ]
+  
     return (
-      <>
-     <Part part={part1.name} exercises={part1.exercises}/>
-      <Part part={part2.name} exercises={part2.exercises}/>
-      <Part part={part3.name} exercises={part3.exercises}/>
-    </>
+      <div>
+        <Header course={course} />
+        <Contents parts={parts} />
+        <Total parts={parts} />
+      </div>
     )
-    
   }
-
-  return (
-    <div>
-      <Header course={course}/>
-      <Contents/>
-      <Total amount={part1.exercises+part2.exercises+part3.exercises}/>
-    </div>
-  )
-}
 
 ReactDOM.render(
   <App />,
