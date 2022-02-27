eda83d8482c047ffcdc5502045204a9387eb149e
Ville Komulainen <ville.m.komulainen@utu.fi>
2022-02-27 14:10:33 +0200

    1.7

--- osa1/feedback/src/index.js
+++ osa1/feedback/src/index.js
@@ -9,39 +9,67 @@ class App extends React.Component {
     this.state = {
       hyvä: 0,
       neutraali: 0,
-      huono: 0
+      huono: 0,
+      kaikki: [],
+      posit: [],
+      summa: 0
     }
+  }
+  klikhyvä = () => {
+    this.setState({
+      hyvä: this.state.hyvä + 1,
+      kaikki: this.state.kaikki.concat(1),
+      summa: this.state.summa + 1,
+      posit: this.state.posit.concat(1)
+    })
+  }
+
+  klikhuono = () => {
+    this.setState({
+      huono: this.state.huono + 1,
+      kaikki: this.state.kaikki.concat(-1),
+      summa: this.state.summa - 1
+    })
+  }
+
+  klikneutraali = () => {
+    
+    this.setState({
+      neutraali: this.state.neutraali + 1,
+      kaikki: this.state.kaikki.concat(0),
+      summa: this.state.summa + 0
+    })
+  }
+
+  laskekarvo  = (props) => {
+    const t = props.arr
+    return(
+      <>
+      {}
+      </>
+    )
   }
   render() {
     return (
       <div>
     <h1>anna palautetta</h1>
-  
-    <button onClick={() => this.setState({ hyvä: this.state.hyvä + 1 })}>
-  hyvä
-</button>
-<button onClick={() => this.setState({ neutraali: this.state.neutraali + 1 })}>
-  neutraali
-</button>
-<button onClick={() => this.setState({ huono: this.state.huono + 1 })}>
-  huono
-</button>
-    <h1>Statistiikkaa</h1>
-    <p> hyvä {this.state.hyvä}</p>
-    <p> neutraali {this.state.neutraali}</p>
-    <p> huono {this.state.huono}</p>
-      </div>
-    
-    )
-  }
+    {console.log(this.state.posit)}
+    <button onClick={this.klikhyvä}>hyvä</button>
+    <button onClick={this.klikneutraali}>neutraali</button>
+    <button onClick={this.klikhuono}>huono</button>
+      <h1>Statistiikkaa</h1>
+      <p> hyvä {this.state.hyvä}</p>
+      <p> neutraali {this.state.neutraali}</p>
+      <p> huono {this.state.huono}</p>
+      <p> keskiarvo {this.state.summa / this.state.kaikki.length}</p>
+      <p> positiivisia {this.state.posit.length / this.state.kaikki.length}%</p>
+        </div>
+      
+      )
+    }
   
   
 }
-
-
-
-
-
 
 
 
